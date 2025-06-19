04edc4c9a9e91afeefaadafe93ea67468fc5ce9d
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2025-06-19 17:14:28 +0100

    feat: Logout if needed and let user login

--- src/index.js
+++ src/index.js
@@ -3,23 +3,51 @@ import Minilog from '@cozy/minilog'
 const log = Minilog('ContentScript')
 Minilog.enable('AxaCCC')
 
+const loginFormUrl = 'https://connect.axa.fr/login'
+const homePageUrl = 'https://espaceclient.axa.fr/#/'
+
 class AxaContentScript extends ContentScript {
   async navigateToLoginForm() {}
 
   onWorkerEvent({ event, payload }) {}
+  onWorkerReady() {}
 
   async ensureAuthenticated({ account }) {
     this.log('info', '🤖 ensureAuthenticated')
     this.bridge.addEventListener('workerEvent', this.onWorkerEvent.bind(this))
+    const credentials = await this.getCredentials()
+    if (!account || !credentials) {
+      await this.ensureNotAuthenticated()
+    }
+    if (!await this.isElementInWorker('#inputLogin')){
+      await this.goto(loginFormUrl)
+      await this.waitForElementInWorker('#inputLogin')
+    }
+    await this.showLoginFormAndWaitForAuthentication()
+    this.log('info', 'ensureAuthenticated - Login successfull !')
+    return true
   }
 
   async ensureNotAuthenticated() {
     this.log('info', '🤖 ensureNotAuthenticated')
+    await this.goto(homePageUrl)
+    // Waiting for elements indicating we're connected or not
+    await this.waitForElementInWorker(`#inputLogin, .btn-disconnect`)
+    const authenticated = await this.runInWorker('checkAuthenticated')
+    if(!authenticated){
+      this.log('info', 'ensureNotAuthenticated - User is already disconnected')
+      return true
+    }
+    await this.runInWorker('click', '.btn-disconnect')
+    await this.waitForElementInWorker('#inputLogin')
+    this.log('info', 'ensureNotAuthenticated - User has been disconnected')
+    return true
   }
 
-  onWorkerReady() {}
 
-  async checkAuthenticated() {}
+  async checkAuthenticated() {
+    return Boolean(document.querySelector('.btn-disconnect'))
+  }
 
   async showLoginFormAndWaitForAuthentication() {
     log.debug('showLoginFormAndWaitForAuthentication start')
